4815dc92f94410766e2f59fbea5d854d9078d097
hmahabub <hm.sayem.14@gmail.com>
2026-09-14 21:19:18 +0600

    2nd commit

--- skytexerp/settings.py
+++ skytexerp/settings.py
@@ -12,7 +12,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1,asgroupbd.com,skytex.asgroupbd.com,www.skytex.asgroupbd.com',
+    default='localhost,127.0.0.1,asgroupbd.com,sky.asgroupbd.com,www.sky.asgroupbd.com',
     cast=Csv(),
 )
 
